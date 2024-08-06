cd4eb0fae915393fbe3ba290fc4b7354373e2be9
Jang-SoHyeon <jangso6593@gmail.com>
2024-08-06 20:12:36 +0900

    realFinal

--- src/hook/SlidingWindow.py
+++ src/hook/SlidingWindow.py
@@ -22,44 +22,29 @@ class SlidingWindow:
         self._request_id = sliding_request_id
 
     def _send_request(self, completion_request):
-        try:
-
-            headers = {
-                'Content-Type': 'application/json; charset=utf-8',
-                'X-NCP-CLOVASTUDIO-API-KEY': self._api_key,
-                'X-NCP-APIGW-API-KEY': self._api_key_primary_val,
-                'X-NCP-CLOVASTUDIO-REQUEST-ID': self._request_id
-            }
-
-            conn = http.client.HTTPSConnection(self._host)
-            conn.request('POST', '/v1/api-tools/sliding/chat-messages/HCX-DASH-001', json.dumps(completion_request),
-                         headers)
-            response = conn.getresponse()
-            result = json.loads(response.read().decode(encoding='utf-8'))
-            logging.warning(result)
-            conn.close()
-            return result
-        except Exception as e:
-            logging.error("er occurred: %s", e)
+        headers = {
+            'Content-Type': 'application/json; charset=utf-8',
+            'X-NCP-CLOVASTUDIO-API-KEY': self._api_key,
+            'X-NCP-APIGW-API-KEY': self._api_key_primary_val,
+            'X-NCP-CLOVASTUDIO-REQUEST-ID': self._request_id
+        }
+        conn = http.client.HTTPSConnection(self._host)
+        conn.request('POST', '/v1/api-tools/sliding/chat-messages/HCX-DASH-001', json.dumps(completion_request),
+                     headers)
+        response = conn.getresponse()
+        result = json.loads(response.read().decode(encoding='utf-8'))
+        conn.close()
+        return result
 
 
     def execute(self, data_list):
         try:
+            messages_json = json.dumps(data_list)
             request_data = json.loads("""{
-              "maxTokens" : 200,
-              "messages" : [ {
-                "role" : "user",
-                "content" : "hello"
-              } ]
-            }""", strict=False)
-            # messages_json = json.dumps(data_list)
-            # request_data = json.loads("""{
-            #     "maxTokens" : 200,
-            #     "messages" : %s
-            # }""" % messages_json, strict=False)
+                "maxTokens" : 200,
+                "messages" : %s
+            }""" % messages_json, strict=False)
             res = self._send_request(request_data)
-            print(request_data)
-            print(res)
             if res['status']['code'] == '20000':
                 return res['result']['messages']
             else:
